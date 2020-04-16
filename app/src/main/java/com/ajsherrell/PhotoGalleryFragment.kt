321fbd7a2034c48c856076a8cc21c7753cd58809
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-16 11:31:03 -0400

    build the retrofit and api instances

--- app/src/main/java/com/ajsherrell/PhotoGalleryFragment.kt
+++ app/src/main/java/com/ajsherrell/PhotoGalleryFragment.kt
@@ -8,10 +8,24 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ajsherrell.photogallery.R
+import com.ajsherrell.photogallery.api.FlickrApi
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 class PhotoGalleryFragment : Fragment() {
 
     private lateinit var photoRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
