86ab7acca5238cb43da4e72a653c96e26b7ddb8d
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-17 12:35:32 -0400

    implement bitmap stream

--- app/src/main/java/com/ajsherrell/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/ajsherrell/photogallery/FlickrFetchr.kt
@@ -1,11 +1,15 @@
 package com.ajsherrell.photogallery
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.ajsherrell.photogallery.api.FlickrApi
 import com.ajsherrell.photogallery.api.FlickrResponse
 import com.ajsherrell.photogallery.api.PhotoResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,6 +22,14 @@ private const val TAG = "FlickrFethcr"
 class FlickrFetchr {
 
     private val flickrApi: FlickrApi
+
+    @WorkerThread
+    fun fetchPhoto(url: String): Bitmap? {
+        val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap = $bitmap from Response!!! = $response")
+        return bitmap
+    }
 
     init {
         val retrofit: Retrofit = Retrofit.Builder()
