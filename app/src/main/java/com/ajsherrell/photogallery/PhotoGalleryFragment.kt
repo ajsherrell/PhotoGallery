97a8819b124036a7f15934f507ecef72721f642f
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-20 12:24:47 -0400

    fixed searchview import bug

--- app/src/main/java/com/ajsherrell/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/ajsherrell/photogallery/PhotoGalleryFragment.kt
@@ -8,7 +8,7 @@ import android.os.Handler
 import android.util.Log
 import android.view.*
 import android.widget.ImageView
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
