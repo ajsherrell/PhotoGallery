762bd7e6fbfb236622a710acd7044f25b47d9cf1
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-20 15:29:28 -0400

    schedule the work request

--- app/src/main/java/com/ajsherrell/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/ajsherrell/photogallery/PhotoGalleryFragment.kt
@@ -16,6 +16,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 
 private const val TAG = "PhotoGalleryFragment"
 
@@ -38,6 +40,12 @@ class PhotoGalleryFragment : Fragment() {
             photoHolder.bindDrawable(drawable)
         }
         lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
+
+        val workRequest = OneTimeWorkRequest
+            .Builder(PollWorker::class.java)
+            .build()
+        WorkManager.getInstance()
+            .enqueue(workRequest)
     }
 
     override fun onCreateView(
