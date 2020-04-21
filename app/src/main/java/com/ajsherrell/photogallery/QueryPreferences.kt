339f434856aa5bd28d72aeeceaa6e3ad4b7b4754
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-21 10:49:54 -0400

    store and retrieve latest photo Id

--- app/src/main/java/com/ajsherrell/photogallery/QueryPreferences.kt
+++ app/src/main/java/com/ajsherrell/photogallery/QueryPreferences.kt
@@ -5,6 +5,7 @@ import android.preference.PreferenceManager
 import androidx.core.content.edit
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
+private const val PREF_LAST_RESULT_ID = "lastResultId"
 
 object QueryPreferences { //"object" indicates a singleton -- means you only need one instance
     // which can be shared across all other components.
@@ -19,6 +20,17 @@ object QueryPreferences { //"object" indicates a singleton -- means you only nee
             .edit {
                 putString(PREF_SEARCH_QUERY, query)
             }
+    }
+
+    fun getLastResultId(context: Context): String {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getString(PREF_LAST_RESULT_ID, "")!!
+    }
+
+    fun setLastResultId(context: Context, lastResultId: String) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit {
+            putString(PREF_LAST_RESULT_ID, lastResultId)
+        }
     }
 
 }
