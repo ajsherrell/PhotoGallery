ad5f8b777a1599c84bd243da245da4a642a9660d
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-21 09:56:46 -0400

    add constraints

--- app/src/main/java/com/ajsherrell/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/ajsherrell/photogallery/PhotoGalleryFragment.kt
@@ -16,6 +16,8 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 
@@ -41,8 +43,12 @@ class PhotoGalleryFragment : Fragment() {
         }
         lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
 
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .build()
         val workRequest = OneTimeWorkRequest
             .Builder(PollWorker::class.java)
+            .setConstraints(constraints)
             .build()
         WorkManager.getInstance()
             .enqueue(workRequest)
