f0da3edc56206a5e2fd28fd8241d94e11b4aec07
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-22 11:33:19 -0400

    cancel notification

--- app/src/main/java/com/ajsherrell/photogallery/VisibleFragment.kt
+++ app/src/main/java/com/ajsherrell/photogallery/VisibleFragment.kt
@@ -1,11 +1,15 @@
 package com.ajsherrell.photogallery
 
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+
+private const val TAG = "VisibleFragment"
 
 abstract class VisibleFragment : Fragment(){
 
@@ -13,6 +17,10 @@ abstract class VisibleFragment : Fragment(){
         override fun onReceive(context: Context?, intent: Intent?) {
             Toast.makeText(requireContext(),
                 "Got a broadcast!!! ${intent?.action}", Toast.LENGTH_LONG).show()
+
+            //if we receive this, we're visible, so cancel it
+            Log.i(TAG, "canceling notification")
+            resultCode = Activity.RESULT_CANCELED
         }
     }
 
