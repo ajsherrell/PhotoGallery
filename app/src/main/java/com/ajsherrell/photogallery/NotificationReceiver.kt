46145be3a4a069b895c4c3c5f5523fe608384902
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-22 11:53:12 -0400

    implement result receiver and set priority

--- app/src/main/java/com/ajsherrell/photogallery/NotificationReceiver.kt
+++ app/src/main/java/com/ajsherrell/photogallery/NotificationReceiver.kt
@@ -1,14 +1,32 @@
 package com.ajsherrell.photogallery
 
+import android.app.Activity
+import android.app.Notification
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
 
 private const val TAG = "NotificationReceiver"
 
 class NotificationReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        Log.i(TAG, "received broadcast!!! ${intent?.action}")
+    override fun onReceive(context: Context, intent: Intent?) {
+        Log.i(TAG, "received result: $resultCode")
+        if (resultCode != Activity.RESULT_OK) {
+            //a foreground activity canceled the broadcast
+            return
+        }
+
+        val requestCode = intent?.getIntExtra(PollWorker.REQUEST_CODE, 0)
+        val notification: Notification? = intent?.getParcelableExtra(PollWorker.NOTIFICATION)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        if (requestCode != null) {
+            if (notification != null) {
+                notificationManager.notify(requestCode, notification)
+            }
+        }
     }
 }
